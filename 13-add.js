c3dee9f0ae21a7f1059a3cb54df5c5f07084f69e
Musa1112 <iliyamusa700@gmail.com>
2023-09-04 17:46:58 +0100

    jp

---
+++ 13-add.js
@@ -0,0 +1,10 @@
+function add(x, y) {
+    // Check if the arguments are numbers
+    if (typeof x !== "number" || typeof y !== "number") {
+      throw new TypeError("Arguments must be numbers");
+    }
+  
+    // Add the two numbers
+    return x + y;
+  }
+  
