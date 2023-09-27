c4b36ba16cac2fb79789b1b57cfe3060b82f462f
Musa1112 <iliyamusa700@gmail.com>
2023-09-27 15:09:00 +0100

    js

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,8 +1,10 @@
-function updateValue(x) {
-    x = Number(x); // Convert x to a number
-    x = 89; // Update the value of x
-    return x;
-  }
-  
-  console.log(updateValue(12));
-  
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+/*
+YOUR CODE HERE
+*/
+myObject.replace(86)
+console.log(myObject);
