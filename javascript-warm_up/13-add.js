90f101bc67113208dec6fcf93905c411edf2318b
Musa1112 <iliyamusa700@gmail.com>
2023-09-27 14:22:03 +0100

    js

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,11 +1,11 @@
 function add(x, y) {
-    // Check if the arguments are numbers
-    if (typeof x !== "number" || typeof y !== "number") {
-      throw new TypeError("Arguments must be numbers");
-    }
-  
-    // Add the two numbers
-    return x + y;
+  // Check if the arguments are numbers
+  if (typeof x !== "number" || typeof y !== "number") {
+    throw new TypeError("Arguments must be numbers");
   }
-  console.log(4, 85)
-  
+
+  // Add the two numbers
+  return x + y;
+}
+
+console.log(add(4, 85)); // 89
