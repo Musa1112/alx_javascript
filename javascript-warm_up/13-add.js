b3bbab93c5bfd4e7d05f8a85ce4efd3e2f7a652a
Musa1112 <iliyamusa700@gmail.com>
2023-09-27 14:31:23 +0100

    operation function

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,11 +1,4 @@
-function add(x, y) {
-  // Check if the arguments are numbers
-  if (typeof x !== "number" || typeof y !== "number") {
-    throw new TypeError("Arguments must be numbers");
-  }
-
-  // Add the two numbers
+function add (x,y) {
   return x + y;
 }
-
-console.log(add(4, 85)); // 89
+console.log(add(4, 85));
