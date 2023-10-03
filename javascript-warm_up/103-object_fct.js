93d26b139e3a61a0abf3f2143bba210549d6533b
Musa1112 <iliyamusa700@gmail.com>
2023-10-03 12:56:35 +0100

    js

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,17 +1,22 @@
-#!/usr/bin/node
-const myObject = {
-    type: 'object',
-    value: 12,
-  };
-  console.log(myObject);
-  
-  myObject.incr = function () {
+class MyObject {
+  constructor(value) {
+    this.value = value;
+  }
+
+  incr() {
     this.value += 1;
-  };
-  
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
+  }
+}
+
+const myObject = new MyObject(12);
+
+console.log(myObject); // { type: 'object', value: 12 }
+
+myObject.incr();
+console.log(myObject); // { type: 'object', value: 13 }
+
+myObject.incr();
+console.log(myObject); // { type: 'object', value: 14 }
+
+myObject.incr();
+console.log(myObject); // { type: 'object', value: 15 }
